a6a9b07fcd17a5529bac9b38154accd6491a34b5
fundDeveloper22 <openfund365@gmail.com>
2024-11-08 14:57:22 +0900

    currentTab clickable

--- apps/web/src/pages/Overview/index.tsx
+++ apps/web/src/pages/Overview/index.tsx
@@ -1,15 +1,141 @@
-import { Helmet } from 'react-helmet-async/lib/index'
+import { InterfaceElementName } from '@uniswap/analytics-events'
+import { TopPoolTable } from 'components/Pools/PoolTable/PoolTable'
+import { TopTokensTable } from 'components/Tokens/TokenTable'
+import { MAX_WIDTH_MEDIA_BREAKPOINT } from 'components/Tokens/constants'
+import { useChainFromUrlParam } from 'constants/chains'
+import RecentTransactions from 'pages/Explore/tables/RecentTransactions'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { StyledInternalLink } from 'theme/components'
+import { Flex, Text } from 'ui/src'
+import { UNIVERSE_CHAIN_INFO } from 'uniswap/src/constants/chains'
+import { FeatureFlags } from 'uniswap/src/features/gating/flags'
+import { useFeatureFlag } from 'uniswap/src/features/gating/hooks'
+import { Trans } from 'uniswap/src/i18n'
+import { UniverseChainId } from 'uniswap/src/types/chains'
 
-export default function OverviewPage() {
-  //   const account = useAccount()
-  //   const contextValue = useCreateTDPContext()
-  //const { tokenColor } = contextValue
+export enum ExploreTab {
+  Tokens = 'tokens',
+  Pools = 'pools',
+  Transactions = 'transactions',
+}
+
+interface Page {
+  title: React.ReactNode
+  key: ExploreTab
+  component: () => JSX.Element
+  loggingElementName: InterfaceElementName
+}
+
+const Pages: Array<Page> = [
+  {
+    title: <Trans i18nKey="common.tokens" />,
+    key: ExploreTab.Tokens,
+    component: TopTokensTable,
+    loggingElementName: InterfaceElementName.EXPLORE_TOKENS_TAB,
+  },
+  {
+    title: <Trans i18nKey="common.pools" />,
+    key: ExploreTab.Pools,
+    component: TopPoolTable,
+    loggingElementName: InterfaceElementName.EXPLORE_POOLS_TAB,
+  },
+  {
+    title: <Trans i18nKey="common.transactions" />,
+    key: ExploreTab.Transactions,
+    component: RecentTransactions,
+    loggingElementName: InterfaceElementName.EXPLORE_TRANSACTIONS_TAB,
+  },
+]
+
+const OverviewPage = ({ initialTab }: { initialTab?: ExploreTab }) => {
+  const tabNavRef = useRef<HTMLDivElement>(null)
+  const isMultichainExploreEnabled = useFeatureFlag(FeatureFlags.MultichainExplore)
+
+  useEffect(() => {
+    if (tabNavRef.current) {
+      const offsetTop = tabNavRef.current.getBoundingClientRect().top + window.scrollY
+      window.scrollTo({ top: offsetTop - 90, behavior: 'smooth' })
+    }
+    // scroll to tab navbar on initial page mount only
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const chainWithoutFallback = useChainFromUrlParam()
+  const chain = useMemo(() => {
+    return isMultichainExploreEnabled
+      ? chainWithoutFallback
+      : chainWithoutFallback ?? UNIVERSE_CHAIN_INFO[UniverseChainId.Mainnet]
+  }, [chainWithoutFallback, isMultichainExploreEnabled])
+
+  const initialKey: number = useMemo(() => {
+    const key = initialTab && Pages.findIndex((page) => page.key === initialTab)
+
+    if (!key || key === -1) {
+      return 0
+    }
+    return key
+  }, [initialTab])
+  const [currentTab, setCurrentTab] = useState(initialKey)
+  const { component: Page } = Pages[currentTab]
 
   return (
-    // <ThemeProvider accent1={tokenColor ?? undefined}>
-    <Helmet>
-      <title>test123</title>
-    </Helmet>
-    //</ThemeProvider>
+    <Flex width="100%" minWidth={320} pt="$spacing48" px="$spacing40" $md={{ p: '$spacing16', pb: 0 }}>
+      {/* <ExploreChartsSection /> */}
+      <Flex
+        ref={tabNavRef}
+        row
+        maxWidth={MAX_WIDTH_MEDIA_BREAKPOINT}
+        mt={0}
+        mx="auto"
+        mb="$spacing4"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+        $lg={{
+          row: false,
+          flexDirection: 'column',
+          mx: 'unset',
+          alignItems: 'flex-start',
+          gap: '$spacing16',
+        }}
+      >
+        <Flex
+          row
+          gap="$spacing24"
+          flexWrap="wrap"
+          justifyContent="flex-start"
+          $md={{ gap: '$spacing16' }}
+          data-testid="explore-navbar"
+        >
+          {Pages.map(({ title, key }, index) => {
+            // hide Transactions tab if no chain is selected
+            return key !== ExploreTab.Transactions || !!chain ? (
+              <StyledInternalLink
+                onClick={() => setCurrentTab(index)}
+                to={
+                  `/explore/${key}` + (chain?.id || chain?.id !== UniverseChainId.Mainnet ? `/${chain?.urlParam}` : '')
+                }
+              >
+                <Text
+                  variant="heading3"
+                  fontSize={28}
+                  $lg={{ fontSize: 24, lineHeight: 32 }}
+                  fontWeight="$book"
+                  color={currentTab === index ? '$neutral1' : '$neutral2'}
+                  cursor="pointer"
+                  animation="quick"
+                  key={key}
+                >
+                  {title}
+                </Text>
+              </StyledInternalLink>
+            ) : null
+          })}
+        </Flex>
+      </Flex>
+      <Page />
+    </Flex>
   )
 }
+
+export default OverviewPage
