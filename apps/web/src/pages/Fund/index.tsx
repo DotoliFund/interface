85d6a1e240122b92a392388b36aac6fc72d10b1d
fundDeveloper22 <openfund365@gmail.com>
2025-02-01 22:07:15 +0900

    create bar chart in fund page

--- apps/web/src/pages/Fund/index.tsx
+++ apps/web/src/pages/Fund/index.tsx
@@ -1,22 +1,30 @@
-import { useAccount } from 'hooks/useAccount'
-import RecentTransactions from 'pages/Fund/tables/RecentTransactions'
-import { useMemo } from 'react'
-// import ChartSection from 'components/Pools/PoolDetails/ChartSection'
-import { Text } from 'ui/src'
-import { Trans } from 'uniswap/src/i18n'
-// import { useDotoliInfoContract } from 'hooks/useContract'
-// import { useSingleCallResult } from 'lib/hooks/multicall'
 import { CurrencyAmount, Token } from '@uniswap/sdk-core'
-import Column from 'components/Column'
-import Row from 'components/Row'
+import FeeBarChart from 'components/BarChart/fee'
+import TokenBarChart from 'components/BarChart/token'
+import VolumeBarChart from 'components/BarChart/volume'
+import { DarkGrayCard } from 'components/Card'
+import Column, { AutoColumn } from 'components/Column'
+import Row, { AutoRow, RowFlat } from 'components/Row'
+import { ToggleElement, ToggleWrapper } from 'components/Toggle/MultiToggle'
+import { MonoSpace } from 'components/shared'
 import { WRAPPED_NATIVE_CURRENCY } from 'constants/tokens'
 import { useFundData } from 'data/Fund/fundData'
 import { useVolumeChartData } from 'data/Fund/volumeChartData'
+import { useAccount } from 'hooks/useAccount'
+import { useDotoliInfoContract } from 'hooks/useContract'
 import { useETHPriceInUSD, useTokensPriceInUSD } from 'hooks/usePools'
+import { useSingleCallResult } from 'lib/hooks/multicall'
 import styled, { useTheme } from 'lib/styled-components'
+import RecentTransactions from 'pages/Fund/tables/RecentTransactions'
+import { useEffect, useMemo, useState } from 'react'
 import { Helmet } from 'react-helmet-async/lib/index'
 import { useParams } from 'react-router-dom'
 import { BREAKPOINTS, ThemeProvider } from 'theme'
+import { Text } from 'ui/src'
+import { Trans } from 'uniswap/src/i18n'
+import { shortenAddress } from 'uniswap/src/utils/addresses'
+import { formatAmount, formatDollarAmount } from 'utils/numbers'
+import { formatTime, unixToDate } from 'utils/time'
 
 export enum ExploreTab {
   Tokens = 'tokens',
@@ -24,11 +32,24 @@ export enum ExploreTab {
   Transactions = 'transactions',
 }
 
-// enum ChartView {
-//   CURRENT_ASSET_TOKENS,
-//   TOKENS,
-//   FEES,
-// }
+enum ChartView {
+  CURRENT_ASSET_TOKENS,
+  TOKENS,
+  FEES,
+}
+
+const ChartWrapper = styled(DarkGrayCard)`
+  border: 1px solid ${({ theme }) => theme.background};
+`
+
+const ToggleRow = styled(RowFlat)`
+  justify-content: flex-end;
+  margin-bottom: 10px;
+
+  @media screen and (max-width: 600px) {
+    flex-direction: row;
+  }
+`
 
 const PageWrapper = styled(Row)`
   padding: 0 16px 52px;
@@ -126,25 +147,25 @@ const Fund = () => {
   //   // eslint-disable-next-line react-hooks/exhaustive-deps
   // }, [])
 
-  // const DotoliInfoContract = useDotoliInfoContract()
-  // const { loading: myManagingFundLoading, result: [myManagingFund] = [] } = useSingleCallResult(
-  //   DotoliInfoContract,
-  //   'managingFund',
-  //   [account.address]
-  // )
-  // const [userIsManager, setUserIsManager] = useState<boolean>(false)
-  // useEffect(() => {
-  //   if (!myManagingFundLoading) {
-  //     setState()
-  //   }
-  //   async function setState() {
-  //     if (myManagingFund && currentPageFund && myManagingFund.toString() === currentPageFund.toString()) {
-  //       setUserIsManager(true)
-  //     } else {
-  //       setUserIsManager(false)
-  //     }
-  //   }
-  // }, [myManagingFundLoading, myManagingFund, currentPageFund])
+  const DotoliInfoContract = useDotoliInfoContract()
+  const { loading: myManagingFundLoading, result: [myManagingFund] = [] } = useSingleCallResult(
+    DotoliInfoContract,
+    'managingFund',
+    [account.address],
+  )
+  const [userIsManager, setUserIsManager] = useState<boolean>(false)
+  useEffect(() => {
+    if (!myManagingFundLoading) {
+      setState()
+    }
+    async function setState() {
+      if (myManagingFund && currentPageFund && myManagingFund.toString() === currentPageFund.toString()) {
+        setUserIsManager(true)
+      } else {
+        setUserIsManager(false)
+      }
+    }
+  }, [myManagingFundLoading, myManagingFund, currentPageFund])
 
   // const { loading: isSubscribedLoading, result: [isSubscribed] = [] } = useSingleCallResult(
   //   DotoliInfoContract,
@@ -168,12 +189,16 @@ const Fund = () => {
   const fundData = useFundData(currentPageFund).data
   const volumeChartData = useVolumeChartData(currentPageFund).data
 
-  // const [view, setView] = useState(ChartView.CURRENT_ASSET_TOKENS)
+  const [view, setView] = useState(ChartView.CURRENT_ASSET_TOKENS)
+
+  // console.log(1,fundData)
+  // console.log(2,fundData?.fund)
+  // console.log(3,fundData?.fund?.feeTokens)
 
   // chart hover index
-  // const [volumeIndexHover] = useState<number | undefined>()
-  // const [tokenIndexHover, setTokenIndexHover] = useState<number | undefined>()
-  // const [feeIndexHover, setFeeIndexHover] = useState<number | undefined>()
+  const [volumeIndexHover, setVolumeIndexHover] = useState<number | undefined>()
+  const [tokenIndexHover, setTokenIndexHover] = useState<number | undefined>()
+  const [feeIndexHover, setFeeIndexHover] = useState<number | undefined>()
 
   const formattedVolumeUSD = useMemo(() => {
     if (volumeChartData && volumeChartData.fundSnapshots) {
@@ -192,27 +217,23 @@ const Fund = () => {
     }
   }, [volumeChartData])
 
-  // const formattedFeeTokens = useMemo(() => {
-  //   if (fundData && fundData.fund) {
-  //     return fundData.fund.feeTokens.map((data: any, index: number) => {
-  //       return {
-  //         token: fundData.fund,
-  //         symbol: fundData.fund.feeSymbols[index],
-  //         amount: fundData.fund.feeTokensAmount[index],
-  //         index,
-  //       }
-  //     })
-  //   } else {
-  //     return []
-  //   }
-  // }, [fundData])
+  const formattedFeeTokens = useMemo(() => {
+    if (fundData && fundData.fund && fundData.fund.feeTokens) {
+      return fundData.fund.feeTokens.map((data: any, index: number) => {
+        return {
+          token: fundData.fund.feeTokens[index],
+          symbol: fundData.fund.feeSymbols[index],
+          amount: fundData.fund.feeTokensAmount[index],
+          index,
+        }
+      })
+    } else {
+      return []
+    }
+  }, [fundData])
 
   const weth9 = account.chainId ? WRAPPED_NATIVE_CURRENCY[account.chainId] : undefined
   const ethPriceInUSDC = useETHPriceInUSD(account.chainId)
-
-  // console.log(1,fundData)
-  // console.log(2,fundData?.fund)
-  // console.log(3,fundData?.fund?.currentTokens)
 
   const currentTokensAmount = useMemo(() => {
     if (account.chainId && fundData && fundData.fund && fundData.fund.currentTokens) {
@@ -345,6 +366,167 @@ const Fund = () => {
               isReversed={isReversed}
               chain={chainInfo?.backendChain.chain}
             /> */}
+
+            <ChartWrapper>
+              <ToggleRow>
+                <ToggleWrapper width="260px">
+                  <ToggleElement
+                    isActive={view === ChartView.CURRENT_ASSET_TOKENS}
+                    fontSize="12px"
+                    onClick={() =>
+                      view === ChartView.CURRENT_ASSET_TOKENS ? {} : setView(ChartView.CURRENT_ASSET_TOKENS)
+                    }
+                  >
+                    <Trans>Current Asset</Trans>
+                  </ToggleElement>
+                  <ToggleElement
+                    isActive={view === ChartView.TOKENS}
+                    fontSize="12px"
+                    onClick={() => (view === ChartView.TOKENS ? {} : setView(ChartView.TOKENS))}
+                  >
+                    <Trans>Tokens</Trans>
+                  </ToggleElement>
+                  {userIsManager ? (
+                    <ToggleElement
+                      isActive={view === ChartView.FEES}
+                      fontSize="12px"
+                      onClick={() => (view === ChartView.FEES ? {} : setView(ChartView.FEES))}
+                    >
+                      <Trans>Fees</Trans>
+                    </ToggleElement>
+                  ) : (
+                    <></>
+                  )}
+                </ToggleWrapper>
+              </ToggleRow>
+              {view === ChartView.CURRENT_ASSET_TOKENS ? (
+                <VolumeBarChart
+                  data={formattedVolumeUSD}
+                  color={accent1}
+                  setIndex={setVolumeIndexHover}
+                  topLeft={
+                    <AutoColumn gap="4px">
+                      {formatDollarAmount(
+                        volumeIndexHover !== undefined && formattedVolumeUSD && formattedVolumeUSD.length > 0
+                          ? formattedVolumeUSD[volumeIndexHover].current
+                          : formattedVolumeUSD && formattedVolumeUSD.length > 0
+                            ? formattedVolumeUSD[formattedVolumeUSD.length - 1].current
+                            : 0,
+                      )}
+                    </AutoColumn>
+                  }
+                  topRight={
+                    <AutoColumn gap="4px" justify="end">
+                      {volumeIndexHover !== undefined ? (
+                        <MonoSpace>
+                          {unixToDate(Number(formattedVolumeUSD[volumeIndexHover].time))} (
+                          {formatTime(formattedVolumeUSD[volumeIndexHover].time.toString(), 8)})
+                        </MonoSpace>
+                      ) : formattedVolumeUSD && formattedVolumeUSD.length > 0 ? (
+                        <MonoSpace>
+                          {unixToDate(formattedVolumeUSD[formattedVolumeUSD.length - 1].time)} (
+                          {formatTime(formattedVolumeUSD[formattedVolumeUSD.length - 1].time.toString(), 8)})
+                        </MonoSpace>
+                      ) : null}
+                    </AutoColumn>
+                  }
+                />
+              ) : view === ChartView.TOKENS ? (
+                <TokenBarChart
+                  data={formattedLatestTokens}
+                  color={accent1}
+                  setIndex={setTokenIndexHover}
+                  topLeft={
+                    <AutoColumn gap="4px">
+                      <AutoRow>
+                        {tokenIndexHover !== undefined && formattedLatestTokens && formattedLatestTokens.length > 0
+                          ? formattedLatestTokens[tokenIndexHover].symbol === 'WETH'
+                            ? 'ETH'
+                            : formattedLatestTokens[tokenIndexHover].symbol
+                          : formattedLatestTokens && formattedLatestTokens.length > 0
+                            ? formattedLatestTokens[0].symbol === 'WETH'
+                              ? 'ETH'
+                              : formattedLatestTokens[0].symbol
+                            : null}
+                        &nbsp;&nbsp;
+                        {tokenIndexHover !== undefined && formattedLatestTokens && formattedLatestTokens.length > 0 ? (
+                          <MonoSpace>{shortenAddress(formattedLatestTokens[tokenIndexHover].token)}</MonoSpace>
+                        ) : formattedLatestTokens && formattedLatestTokens.length > 0 ? (
+                          <MonoSpace>{shortenAddress(formattedLatestTokens[0].token)}</MonoSpace>
+                        ) : null}
+                      </AutoRow>
+                      {tokenIndexHover !== undefined && formattedLatestTokens && formattedLatestTokens.length > 0
+                        ? formatDollarAmount(formattedLatestTokens[tokenIndexHover].volume)
+                        : formattedLatestTokens && formattedLatestTokens.length > 0
+                          ? formatDollarAmount(formattedLatestTokens[0].volume)
+                          : null}
+                      <br />
+                    </AutoColumn>
+                  }
+                  topRight={
+                    <AutoColumn gap="4px" justify="end">
+                      <MonoSpace>
+                        {unixToDate(nowDate)} ({formatTime(nowDate.toString(), 8)})
+                      </MonoSpace>
+                      <MonoSpace>
+                        {tokenIndexHover !== undefined && formattedLatestTokens && formattedLatestTokens.length > 0
+                          ? formatAmount(formattedLatestTokens[tokenIndexHover].amount)
+                          : formattedLatestTokens && formattedLatestTokens.length > 0
+                            ? formatAmount(formattedLatestTokens[0].amount)
+                            : null}
+                        <br />
+                      </MonoSpace>
+                    </AutoColumn>
+                  }
+                />
+              ) : userIsManager && view === ChartView.FEES ? (
+                <FeeBarChart
+                  data={formattedFeeTokens}
+                  color={accent1}
+                  setIndex={setFeeIndexHover}
+                  topLeft={
+                    <AutoColumn gap="4px">
+                      <AutoRow>
+                        <>
+                          {feeIndexHover !== undefined && formattedFeeTokens && formattedFeeTokens.length > 0
+                            ? formattedFeeTokens[feeIndexHover].symbol === 'WETH'
+                              ? 'ETH'
+                              : formattedFeeTokens[feeIndexHover].symbol
+                            : formattedFeeTokens && formattedFeeTokens.length > 0
+                              ? formattedFeeTokens[0].symbol === 'WETH'
+                                ? 'ETH'
+                                : formattedFeeTokens[0].symbol
+                              : null}
+                          &nbsp;&nbsp;
+                        </>
+                        <>
+                          {feeIndexHover !== undefined && formattedFeeTokens && formattedFeeTokens.length > 0 ? (
+                            <MonoSpace>{shortenAddress(formattedFeeTokens[feeIndexHover].token)}</MonoSpace>
+                          ) : formattedFeeTokens && formattedFeeTokens.length > 0 ? (
+                            <MonoSpace>{shortenAddress(formattedFeeTokens[0].token)}</MonoSpace>
+                          ) : null}
+                        </>
+                      </AutoRow>
+                      <>
+                        {feeIndexHover !== undefined && formattedFeeTokens && formattedFeeTokens.length > 0 ? (
+                          formatAmount(formattedFeeTokens[feeIndexHover].amount)
+                        ) : formattedFeeTokens && formattedFeeTokens.length > 0 ? (
+                          formatAmount(formattedFeeTokens[0].amount)
+                        ) : (
+                          <br />
+                        )}
+                      </>
+                    </AutoColumn>
+                  }
+                  topRight={
+                    <AutoColumn gap="4px" justify="end">
+                      {unixToDate(nowDate)} ({formatTime(nowDate.toString(), 8)})
+                      <br />
+                    </AutoColumn>
+                  }
+                />
+              ) : null}
+            </ChartWrapper>
           </Column>
           <HR />
           <Text
--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -1,1 +1,45 @@
 export { escapeRegExp } from './escapeRegExp'
+
+// // returns the checksummed address if the address is valid, otherwise returns false
+// function isAddress(value: any): string | false {
+//   try {
+//     return getAddress(value)
+//   } catch {
+//     return false
+//   }
+// }
+
+// // shorten the checksummed version of the input address to have 0x + 4 characters at start and end
+// function shortenAddress(address: string, chars = 4): string {
+//   const parsed = isAddress(address)
+//   if (!parsed) {
+//     throw Error(`Invalid 'address' parameter '${address}'.`)
+//   }
+//   return `${parsed.substring(0, chars + 2)}...${parsed.substring(42 - chars)}`
+// }
+
+export function getEtherscanLink(
+  data: string | undefined,
+  type: 'transaction' | 'token' | 'address' | 'block',
+): string {
+  if (!data) {
+    return ''
+  }
+  const prefix = `https://etherscan.io`
+
+  switch (type) {
+    case 'transaction': {
+      return `${prefix}/tx/${data}`
+    }
+    case 'token': {
+      return `${prefix}/token/${data}`
+    }
+    case 'block': {
+      return `${prefix}/block/${data}`
+    }
+    case 'address':
+    default: {
+      return `${prefix}/address/${data}`
+    }
+  }
+}
